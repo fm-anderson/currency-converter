d7ed8fc1751403a022ce2e0b915296be8e1e7b8a
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-07-12 17:47:32 -0400

    fix: making Home Page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,11 @@ function Home() {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img src={heroImage} className="w-auto rounded-lg" />
         <div>
-          <h1 className="text-3xl lg:text-7xl font-bold">
+          <h1 className="text-3xl text-center md:text-start lg:text-7xl font-bold">
             Compare Currency{' '}
             <span className="text-[#F7D465]">Prices for Free!</span>
           </h1>
-          <p className="py-6">
+          <p className="py-6 text-center md:text-start">
             Get accurate currency exchange rates for free. Here you'll always
             have the most up-to-date information.
           </p>
